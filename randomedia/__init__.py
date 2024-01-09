5c70dbfc079ecc112fbb8bf0cd0b859d3a995276
Zane Taylor <zanetaylor@pm.me>
2024-01-09 04:23:34 +0000

    Remove unused env var

--- randomedia/__init__.py
+++ randomedia/__init__.py
@@ -23,12 +23,13 @@ def create_app(test_config=None):
 
     @app.route('/')
     def index():
-        url = os.getenv("MEDIA_ENDPOINT")
-        pw = os.getenv("MEDIA_ENDPOINT_PW")
 
-        mr = requests.get(url + '?ls&pw=' + pw, timeout=1)
+        api_url = os.getenv("MEDIA_ENDPOINT_URL")
+        api_category = os.getenv("MEDIA_DEFAULT_CATEGORY")
 
-        media = mr.json()
+        api_request = requests.get(api_url + api_category + '?ls', timeout=1)
+        media = api_request.json()
+        
         num_media = len(media['files'])
         num_rand = randint(0,num_media-1)
 
@@ -39,8 +40,30 @@ def create_app(test_config=None):
         else:
             rand_media_type = 'video'
 
-        rand_media_url = url + media['files'][num_rand]['href']
+        rand_media_url = api_url + api_category + '/' + media['files'][num_rand]['href']
 
         return render_template('home.html', media_type=rand_media_type,media_url=rand_media_url)
     
+    @app.route('/<category>')
+    def from_category(category):
+
+        api_url = os.getenv("MEDIA_ENDPOINT_URL")
+
+        api_request = requests.get(api_url + category + '?ls', timeout=1)
+        media = api_request.json()
+
+        num_media = len(media['files'])
+        num_rand = randint(0,num_media-1)
+
+        rand_media_ext = media['files'][num_rand]['ext']
+
+        if rand_media_ext in ['jpg','png','webp','gif']:
+            rand_media_type = 'image'
+        else:
+            rand_media_type = 'video'
+
+        rand_media_url = api_url + category + '/' + media['files'][num_rand]['href']
+
+        return render_template('home.html', media_type=rand_media_type,media_url=rand_media_url)
+
     return app
